f747187da943058f2f12e048f842a56f96162ddc
lassepe <lasse.peters@tuhh.de>
2019-02-06 15:09:11 -0800

    test_belief_updater in a loop

--- src/debug.jl
+++ src/debug.jl
@@ -28,7 +28,7 @@ function get_test_problem()
   # create some test problem
   pomdp_exact = HSPOMDP(sensor=ExactPositionSensor(), mdp=HSMDP(transition_model=PControlledHumanTransition()))
   pomdp_noisy = HSPOMDP(sensor=NoisyPositionSensor([0.3, 0.3, 0.3]), mdp=HSMDP(transition_model=PControlledHumanAWGNTransition()))
-  rng = MersenneTwister(7)
+  rng = MersenneTwister(1)
 
   return pomdp_exact, pomdp_noisy, rng
 end
@@ -51,12 +51,14 @@ function simulate_with_policy(n_runs=1)
   close(win)
 end
 
-function test_belief_updater()
+function test_belief_updater(n_runs::Int=1)
   exact_pomdp, noisy_pomdp, rng = get_test_problem()
   # the blief updater is run with a stocahstic version of the world
   belief_updater = SIRParticleFilter(noisy_pomdp, 1000, rng=rng)
   # the policy plannes without a model as it is always the same action
   policy = FunctionPolicy(x->HSAction())
   # the simulator uses the exact dynamics (not known to the belief_updater)
-  makegif(exact_pomdp, policy, belief_updater, filename=joinpath(@__DIR__, "../renderings/out.gif"), extra_initial=true, rng=rng, max_steps=100, show_progress=true)
+  for i_run in 1:n_runs
+    makegif(exact_pomdp, policy, belief_updater, filename=joinpath(@__DIR__, "../renderings/out$i_run.gif"), extra_initial=true, rng=rng, max_steps=100, show_progress=true)
+  end
 end
