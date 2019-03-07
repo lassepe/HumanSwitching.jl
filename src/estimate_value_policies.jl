7db8a16410316c44981ee8f4fdb8a57a909d1c0d
lassepe <lasse.peters@tuhh.de>
2019-03-07 10:59:52 -0800

    Fix reward estiamte

--- src/estimate_value_policies.jl
+++ src/estimate_value_policies.jl
@@ -29,8 +29,7 @@ function free_space_estimate(mdp::HSMDP, s::HSState, steps::Int=0)::Float64
 
     # stage cost
     reward_estimate::Float64 = remaining_step_estimate > 0 ?
-    sum(rm.living_penalty*(rm.discount_factor^i) for i in 1:remaining_step_estimate) :
-    0.0
+    sum(rm.living_penalty*(rm.discount_factor^(i-1)) for i in 1:remaining_step_estimate) : 0.0
     # terminal cost for reaching the goal
     reward_estimate += rm.target_reached_reward*(rm.discount_factor^remaining_step_estimate)
 
