86b0077af2ea63ea8a5fe75396ad710c202b08c1
lassepe <lasse.peters@tuhh.de>
2019-03-14 19:12:58 -0700

    terminal states have value 0

--- src/estimate_value_policies.jl
+++ src/estimate_value_policies.jl
@@ -23,15 +23,20 @@ const robot_max_speed = maximum(a[1] for a in HSActionSpace())
 
 # depth is the solver `depth` parameter less the number of timesteps that have already passed (it can be ignored in many cases)
 function free_space_estimate(mdp::HSMDP, s::HSState, steps::Int=0)::Float64
+    if isterminal(mdp, s)
+        return 0
+    end
     rm = reward_model(mdp)
     dist = robot_dist_to_target(mdp, s, p=1)
     remaining_step_estimate = div(dist, robot_max_speed)
 
+    reward_estimate::Float64 = 0
     # stage cost
-    reward_estimate::Float64 = remaining_step_estimate > 0 ?
-    sum(rm.living_penalty*(rm.discount_factor^(i-1)) for i in 1:remaining_step_estimate) : 0.0
-    # terminal cost for reaching the goal
-    reward_estimate += rm.target_reached_reward*(rm.discount_factor^remaining_step_estimate)
+    if remaining_step_estimate > 0
+        reward_estimate += sum(rm.living_penalty*(rm.discount_factor^(i-1)) for i in 1:remaining_step_estimate)
+        # terminal cost for reaching the goal
+        reward_estimate += rm.target_reached_reward*(rm.discount_factor^(remaining_step_estimate))
+    end
 
     return reward_estimate
 end
