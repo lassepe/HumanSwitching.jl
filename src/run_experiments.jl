b9a1f5bd52c2b08b47e77bc45f9ad3d01e3fa983
lassepe <lasse.peters@tuhh.de>
2019-03-17 18:37:11 -0700

    Smiple script for running experiments and saving to results

---
+++ src/run_experiments.jl
@@ -0,0 +1,28 @@
+using CSV
+using Dates
+using DataFrames
+using Distributed
+
+const desired_nworkers = 1
+
+if nworkers() != desired_nworkers
+    wait(rmprocs(workers()))
+    addprocs(desired_nworkers)
+end
+
+@info "Started $(nworkers())"
+@info "Precompiling simulation code..."
+
+include("debug.jl")
+
+function main()
+    @info "Running simulations..."
+    data = parallel_sim(1:3)
+    @info "Writing data..."
+    result_dir = realpath("$(@__DIR__)/../results/")
+    file_name = "sim_results-$(gethostname())-$(now()).csv"
+    file = CSV.write(joinpath(result_dir, file_name), data)
+    @info "All done! Wrote results to $file."
+end
+
+main()
