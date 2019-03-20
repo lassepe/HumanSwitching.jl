88c356101822160a69d9b555511dce7c11c6cf45
lassepe <lasse.peters@tuhh.de>
2019-03-19 22:56:15 -0700

    Minor change to plotting

--- src/plotting.jl
+++ src/plotting.jl
@@ -19,17 +19,18 @@ function plot_points(data::DataFrame)
     value = plot(data, x=:planner_hbm_key, y=:discounted_reward, color=:planner_hbm_key, violin_plot_appearance...)
 	compute = plot(data, x=:planner_hbm_key, y=:median_planner_time, color=:planner_hbm_key, violin_plot_appearance...)
 
-    success_rate =  plot(data, xgroup=:planner_hbm_key, x=:final_state_type, Geom.subplot_grid(Geom.histogram(density=true)),
+    success_rate =  plot(data, xgroup=:planner_hbm_key, x=:final_state_type, Geom.subplot_grid(Geom.histogram),
                          Gadfly.Theme(major_label_font_size=8pt, minor_label_font_size=8pt, key_position=:none))
 
-	display(Gadfly.title(vstack(scatter,
+    final_plot = Gadfly.title(vstack(scatter,
                                 hstack(value, compute),
                                 success_rate),
                          """
                          Problem Instance: $(first(data[:pi_key]))
                          True Human Model: $(first(data[:simulation_hbm_key]))
                          """)
-           )
+
+	display(final_plot)
 end
 
 simplify_hbm_name(s::String) = string(split(s, "Human")...)
