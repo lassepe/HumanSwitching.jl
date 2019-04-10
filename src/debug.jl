5392cfd91cacb26303d3fb71d987275e0be89207
lassepe <lasse.peters@tuhh.de>
2019-04-10 15:48:50 -0700

    free_space_estimate is saved in data frame for relative reference

--- src/debug.jl
+++ src/debug.jl
@@ -347,7 +347,8 @@ function test_parallel_sim(runs::UnitRange{Int}, solver_setup_key::String="POMCP
                 :hist_validation_hash => validation_hash(hist),
                 :median_planner_time => median(ai[:planner_cpu_time_us] for ai in eachstep(hist, "ai")),
                 :median_updater_time => median(ui[:updater_cpu_time_us] for ui in eachstep(hist, "ui")),
-                :final_state_type => final_state_type(problem(sim), hist)]
+                :final_state_type => final_state_type(problem(sim), hist),
+                :free_space_estimate => free_space_estimate(mdp(problem(sim)), first(collect(s for s in eachstep(hist, "s"))))]
     end
     return data
 end
