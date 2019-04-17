c17d288fcfbb09beb9ecf9fe090a11f489d9aa36
lassepe <lasse.peters@tuhh.de>
2019-04-16 19:01:52 -0700

    Started implementing minimal test case for ProbObstacleSolver

--- src/debug.jl
+++ src/debug.jl
@@ -1,8 +1,23 @@
-using Distributed
+using Pkg
+using Revise
+using HumanSwitching
 
-@everywhere begin
-    using Pkg
-    Pkg.activate(".")
-    using Revise
-    using HumanSwitching
+using Random
+using ParticleFilters
+
+decoupled(s::HSState) = (human_pos(s), hbs(s))::HSHumanState
+
+function test_prob_obstacle()
+    rng = MersenneTwister(1)
+    pomdp = HSPOMDP(ExactPositionSensor(), gen_hsmdp(rng))
+
+    s = initialstate(pomdp, rng)
+    dump(s)
+    sd = decoupled(s)
+    dump(sd)
+
+    # human_predictor = PredictModel{HSHumanState}
+    # bp = BeliefPropagator()
+
+    # solver = ProbObstacleSolver()
 end
