6b50c4cf0ed2f6c6de66d843018551221ad840c7
lassepe <lasse.peters@tuhh.de>
2019-04-19 13:21:48 -0700

    Add test for adversarial sampling

--- test/runtests.jl
+++ test/runtests.jl
@@ -162,10 +162,14 @@ end
     end
 end
 
-# @testset "Adversarial Sampling" begin
-# TODO, run on parallel_sim
-# end
-#
+@testset "Adversarial Sampling" begin
+    test_data = parallel_sim(1:50, "StraightToGoal";
+                             problem_instance_keys=["CornerGoalsNonTrivial"],
+                             ignore_uncommited_changes=true)
+    # any non-trivial scenario should not be solved succesfully with the straight to goal policy
+    @test all(test_data.final_state_type .== "failure")
+end
+
 # @testset "SearchTest" begin
 # TODO, run some search problems with known optimal solution
 # end
