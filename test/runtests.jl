50c023789c6006db2e24e33bffef9b1d93b121b6
lassepe <lasse.peters@tuhh.de>
2019-04-19 15:53:52 -0700

    Implement unit tests for search in simple grid world

--- test/runtests.jl
+++ test/runtests.jl
@@ -15,6 +15,7 @@ using POMDPModelTools
 using POMDPPolicies
 using POMDPSimulators
 using POMDPGifs
+using Parameters
 
 macro testblock(ex)
     quote
@@ -170,9 +171,76 @@ end
     @test all(test_data.final_state_type .== "failure")
 end
 
-# @testset "SearchTest" begin
-# TODO, run some search problems with known optimal solution
-# end
+
+# defining a simple test search problem
+struct GridPosition
+    x_idx::Int
+    y_idx::Int
+end
+
+struct GridAction
+    dx::Int
+    dy::Int
+end
+
+apply_action(s::GridPosition, a::GridAction) = GridPosition(s.x_idx + a.dx, s.y_idx + a.dy)
+
+@with_kw struct GridNavigationProblem <: SearchProblem{GridPosition}
+    grid_dimensions::Tuple{Int, Int} = (10, 10)
+    obstacles::Vector{GridPosition} = []
+    aspace::Vector{GridAction} = [GridAction(0, 0),
+                                  GridAction(1, 0), GridAction(-1, 0),
+                                  GridAction(0, 1), GridAction(0, -1)]
+end
+
+HS.start_state(p::GridNavigationProblem) = GridPosition(1, 1)
+HS.is_goal_state(p::GridNavigationProblem, s::GridPosition) = s == GridPosition(p.grid_dimensions...)
+on_grid(p::GridNavigationProblem, s::GridPosition) = (1 <= s.x_idx <= p.grid_dimensions[1]) && (1 <= s.y_idx <= p.grid_dimensions[2])
+
+function HS.successors(p::GridNavigationProblem, s::GridPosition)
+    successors::Vector{Tuple{GridPosition, GridAction, Int}} = []
+    sizehint!(successors, length(p.aspace))
+    for a in p.aspace
+        sp = apply_action(s, a)
+        if !on_grid(p, sp) || sp in p.obstacles
+            continue
+        end
+        # we are solving a minimum time problem, the step cost is always 1
+        push!(successors, (sp, a, 1))
+    end
+    return successors
+end
+
+@testset "SearchTest" begin
+    # tuples of (dimensions, obstacles, optimal_nsteps, solvable)
+    test_setups::Vector{Tuple{Tuple{Int, Int}, Vector, Int, Bool}} =
+    [
+     # 1x1 grid, no action needed to reach the goal
+     ((1, 1), [], 0, true),
+     # empty 10x10 grid, solvable in 18 steps
+     ((10, 10), [], 18, true),
+     # 10x10 grid with wall in the middle that has a gap, solvable in 18 steps
+     ((10, 10), [GridPosition(5, y) for y in 1:9], 18, true),
+     # 10x10 grid with wall all the way, not solvable
+     ((10, 10), [GridPosition(5, y) for y in 1:10], -1, false),
+     # 5x5 grid with two walls, gap at top and bottom, solvable in 16 steps
+     ((5, 5), [[GridPosition(2, y) for y in 1:4]...,
+               [GridPosition(4, y) for y in 2:5]...], 16, true)
+    ]
+
+    for (test_dims, test_obstacles, optimal_nsteps, solvable) in test_setups
+        p = GridNavigationProblem(grid_dimensions=test_dims,
+                                  obstacles=test_obstacles)
+        # using the manhattan distance as a heuristic
+        h = (s::GridPosition) -> abs(s.x_idx - p.grid_dimensions[1]) + abs(s.y_idx - p.grid_dimensions[2])
+        if solvable
+            aseq, sseq = astart_search(p, h)
+            @test length(aseq) == optimal_nsteps
+        else
+            @test_throws ErrorException astart_search(p, h)
+        end
+    end
+end
 
 @testset "Type Inference tests" begin
     # external state
