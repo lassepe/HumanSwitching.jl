7ebc0c813866bd98c98f105256f113509a686ff9
lassepe <lasse.peters@tuhh.de>
2019-04-25 13:11:35 -0700

    Setup experiments to collect some timing data for planning vs prediction time

--- src/run_experiments.jl
+++ src/run_experiments.jl
@@ -3,7 +3,7 @@ using Dates
 using DataFrames
 using Distributed
 
-const desired_nworkers = 17
+const desired_nworkers = 25
 
 if nworkers() != desired_nworkers
     wait(rmprocs(workers()))
@@ -20,9 +20,9 @@ end
 end
 
 function main()
-    for solver_key in ["POMCPOW"]
+    for solver_key in ["ProbObstacles"]
         @info "Running simulations..."
-        data = parallel_sim(1:1000, solver_key; problem_instance_keys=["CornerGoalsNonTrivial"])
+        data = parallel_sim(1:200, solver_key; problem_instance_keys=["CornerGoalsNonTrivial"])
         @info "Writing data..."
         result_dir = realpath("$(@__DIR__)/../results/")
         file_name = "sim_results-$(gethostname())-$(now())-$(solver_key).csv"
