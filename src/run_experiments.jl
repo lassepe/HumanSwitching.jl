b64821637b0261ffb935ad438659b38ed32bd082
lassepe <lasse.peters@tuhh.de>
2019-04-25 10:35:22 -0700

    Re-Setup POMCPOW experiments

--- src/run_experiments.jl
+++ src/run_experiments.jl
@@ -20,7 +20,7 @@ end
 end
 
 function main()
-    for solver_key in ["GapChecking", "ProbObstacles"]
+    for solver_key in ["POMCPOW"]
         @info "Running simulations..."
         data = parallel_sim(1:1000, solver_key; problem_instance_keys=["CornerGoalsNonTrivial"])
         @info "Writing data..."
