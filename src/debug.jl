5c3cd8597591318ba0f835bbf5f850ad5e2dd335
lassepe <lasse.peters@tuhh.de>
2019-04-24 23:44:38 -0700

    Comparing models

--- src/debug.jl
+++ src/debug.jl
@@ -3,4 +3,12 @@ using HumanSwitching
 using StaticArrays
 using LinearAlgebra
 
-# Intersection of line with circle
+function compare_models()
+    data_switching = parallel_sim(1:3, "GapChecking"; ignore_uncommited_changes=true)
+    data_probObstacle = parallel_sim(1:3, "ProbObstacles"; ignore_uncommited_changes=true)
+
+    println("Discounted reward diff: switching - probObstacles")
+    display(data_switching.discounted_reward .- data_probObstacle.discounted_reward)
+
+    return (data_switching, data_probObstacle)
+end
