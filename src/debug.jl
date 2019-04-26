62e51f79e3a0b198f7a0978e14e647cede5d4af7
lassepe <lasse.peters@tuhh.de>
2019-04-26 15:16:50 -0700

    Add profile code for prob obstacles

--- src/debug.jl
+++ src/debug.jl
@@ -1,7 +1,9 @@
 using Revise
 using HumanSwitching
+const HS=HumanSwitching
 using StaticArrays
 using LinearAlgebra
+
 
 function compare_models()
     data_switching = parallel_sim(1:3, "GapChecking"; ignore_uncommited_changes=true)
@@ -11,4 +13,33 @@ function compare_models()
     display(data_switching.discounted_reward .- data_probObstacle.discounted_reward)
 
     return (data_switching, data_probObstacle)
+end
+
+using POMDPs, POMDPSimulators, POMDPModelTools
+using Profile
+using ProfileView
+using BenchmarkTools
+
+function profile_prob_obstacles()
+    sim = HS.setup_test_scenario("CornerGoalsNonTrivial",
+                                 "HumanMultiGoalBoltzmann_all_goals",
+                                 "HumanMultiGoalBoltzmann_all_goals",
+                                 "ProbObstacles",
+                                 1)
+
+    hist = simulate(sim)
+
+    b = first(collect(eachstep(hist, "b")))
+    policy = sim.policy
+
+    # run once for compilation
+    a, info = action_info(policy, b)
+    display(@benchmark action_info($policy, $b))
+
+    # run the actual profiling
+    Profile.clear()
+    @profile begin
+        a, info = action_info(policy, b)
+    end
+    ProfileView.view()
 end
