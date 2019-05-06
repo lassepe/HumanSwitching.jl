72eff1cf6126ad7b26d437cbce41895cf4e18175
lassepe <lasse.peters@tuhh.de>
2019-05-05 22:35:29 -0700

    First working SLURM setup

--- src/run_experiments.jl
+++ src/run_experiments.jl
@@ -2,15 +2,26 @@ using CSV
 using Dates
 using DataFrames
 using Distributed
+using Pkg
+Pkg.instantiate()
 
 const desired_nworkers = 10
 
-if nworkers() != desired_nworkers
-    wait(rmprocs(workers()))
-    addprocs(desired_nworkers)
+const IN_SLURM = "SLURM_JOBID" in keys(ENV)
+IN_SLURM && using ClusterManagers
+
+if IN_SLURM
+    pids = addprocs_slurm(parse(Int, ENV["SLURM_NTASKS"]))
+    print("\n")
+else
+    if nworkers() != desired_nworkers
+        wait(rmprocs(workers()))
+        pids = addprocs(desired_nworkers)
+    end
 end
 
-@info "Started $(nworkers())"
+
+@info "Started $(nworkers()) workers..."
 @info "Precompiling simulation code..."
 
 @everywhere begin
@@ -22,7 +33,7 @@ end
 function main()
     solver_keys = ["POMCPOW"]
     @info "Running simulations..."
-    data = parallel_sim(1:1000, solver_keys; problem_instance_keys=["CornerGoalsNonTrivial"])
+    data = parallel_sim(1:100, solver_keys; problem_instance_keys=["CornerGoalsNonTrivial"], ignore_uncommited_changes=true)
     @info "Writing data..."
     result_dir = realpath("$(@__DIR__)/../results/")
     file_name = "sim_results-$(gethostname())-$(now())-$(join(solver_keys, "_")).csv"
@@ -31,3 +42,5 @@ function main()
 end
 
 main()
+
+rmprocs(pids)
