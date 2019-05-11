a04c684310d39d9594343cee37f8d73b93152063
lassepe <lasse.peters@tuhh.de>
2019-05-11 15:51:36 -0700

    MT 250

--- src/simulation_utils.jl
+++ src/simulation_utils.jl
@@ -258,11 +258,19 @@ end
 
 function solver_setup_map(planner_setup::PlannerSetup, planner_model::HSModel, rng::MersenneTwister)
     return Dict{String, Union{Solver, Policy}}(
-                                               "DESPOTConstLBK15" => begin
+                                               "DESPOTConstMT200" => begin
                                                    default_policy = StraightToGoal(planner_model)
                                                    bounds = IndependentBounds(-900, free_space_estimate,
                                                                               check_terminal=true, consistency_fix_thresh=1e-8)
-                                                   DESPOTSolver(K=15, D=70, max_trials=typemax(Int), T_max=0.3,
+                                                   DESPOTSolver(K=15, D=70, max_trials=200, T_max=Inf,
+                                                                bounds=bounds, rng=deepcopy(rng), tree_in_info=false,
+                                                                default_action=default_policy)
+                                               end,
+                                               "DESPOTConstMT250" => begin
+                                                   default_policy = StraightToGoal(planner_model)
+                                                   bounds = IndependentBounds(-900, free_space_estimate,
+                                                                              check_terminal=true, consistency_fix_thresh=1e-8)
+                                                   DESPOTSolver(K=15, D=70, max_trials=250, T_max=Inf,
                                                                 bounds=bounds, rng=deepcopy(rng), tree_in_info=false,
                                                                 default_action=default_policy)
                                                end,
